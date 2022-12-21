9e730e239a55c61f650a409b6194a3c7458da94b
OzzieRocks <121136413+OzzieRocks@users.noreply.github.com>
2022-12-21 14:36:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function till_selection() {
     var till_options = document.getElementById("till_list");
     document.getElementById("selected").value = till_options.options[till_options.selectedIndex].text;
 }
+
+function submit_button() {
+    <!-- Verify Submission -->
+    alert("Verify the information before submitting...")
+    
+    
+}
