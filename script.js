694546820c6a5f596172520ee93e8493f65fb8b3
OzzieRocks <121136413+OzzieRocks@users.noreply.github.com>
2023-01-10 11:36:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ function year_selection() {
 function submit_form() {
     <!-- Verify Submission -->
     <!-- Get all inputs -->
-    var register = documnet.getElementById("till_list");
-    var month = documnet.getElementById("month_list");
-    var day = documnet.getElementById("day_list");
-    var year = documnet.getElementById("year_list");
-    var name = documnet.getElementById("name_input");
-    var starting_cash = documnet.getElementById("starting_cash_amount_input");
-    var change = documnet.getElementById("change_input");
-    var ones = documnet.getElementById("number_of_1_input");
-    var fives = documnet.getElementById("number_of_5_input");
-    var tens = documnet.getElementById("number_of_10_input");
-    var twenties = documnet.getElementById("number_of_20_input");
-    var fifties = documnet.getElementById("number_of_50_input");
-    var hundreds = documnet.getElementById("number_of_100_input");
+    var register = document.getElementById("till_list");
+    var month = document.getElementById("month_list");
+    var day = document.getElementById("day_list");
+    var year = document.getElementById("year_list");
+    var name = document.getElementById("name_input");
+    var starting_cash = document.getElementById("starting_cash_amount_input");
+    var change = document.getElementById("change_input");
+    var ones = document.getElementById("number_of_1_input");
+    var fives = document.getElementById("number_of_5_input");
+    var tens = document.getElementById("number_of_10_input");
+    var twenties = document.getElementById("number_of_20_input");
+    var fifties = document.getElementById("number_of_50_input");
+    var hundreds = document.getElementById("number_of_100_input");
     
     
 }
